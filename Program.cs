f6303f512ffe83080a37242923c1a431e3145bde
Michael Welch <michael.g.welch@jci.com>
2018-10-24 11:14:20 -0500

    Create skeleton program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tutorial_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
