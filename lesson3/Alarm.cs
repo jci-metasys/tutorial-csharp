9dc9cedede1af99501193a9f1c062bedd3965699
jevanssp <savannah.pamela.evans@jci.com>
2019-08-02 12:05:26 -0500

    Renamed lesson5 to lesson3. Cleaned code and updated version number for endpoints.

---
+++ lesson3/Alarm.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+namespace lesson3
+{
+    public class AlarmCollection
+    {
+        private string _next;
+        private string _previous;
+        private string _self;
+
+        public int Total { get; set; }
+
+        private static string StripLeadingSlash(string value)
+        {
+            return string.IsNullOrEmpty(value) || value[0] != '/' ? value : value.Substring(0);
+        }
+
+        public string Next
+        {
+            get => _next;
+            set => _next = StripLeadingSlash(value);
+        }
+
+        public string Previous 
+        {  
+            get => _previous;
+            set => _previous = StripLeadingSlash(value);
+        }
+
+        public string Self
+        {
+            get => _self;
+            set => _self = StripLeadingSlash(value);
+        }
+
+        public IList<Alarm> Items { get; set; }
+
+        public Alarm this[int index] => Items[index];
+    }
+
+    public class Alarm
+    {
+        public Guid Id { get; set; }
+        public string ItemReference { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string @Object { get; set; }
+        public int Priority { get; set; }
+        public bool IsAcknowledged { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; }
+    }
+}
--- lesson3/Program.cs
+++ lesson3/Program.cs
@@ -1,58 +1,43 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
-using JohnsonControls.Net.Http;
-using Newtonsoft.Json.Linq;
+using Flurl.Http;
+using Newtonsoft.Json;
 
 namespace lesson3
 {
     public class Program
     {
-
         public static async Task Main(string[] args)
         {
             var username = args[0];
             var password = args[1];
             var hostname = args[2];
 
-            var handler = new HttpClientRedirectHandler();
-
-            using (var client = new HttpClient(handler) {BaseAddress = new Uri($"https://{hostname}/api/v1")})
+            string accessToken;
+            using (var client = new FlurlClient($"https://{hostname}/api/v2"))
             {
-                // Login - constructing a payload that looks like { "username": "thename', "password": "thepassword" }
-                var loginMessage = $"{{ 'username': '{username}', 'password': '{password}' }}";
-
-                var loginContent = new StringContent(loginMessage,
-                    Encoding.UTF8,
-                    "application/json");
-
-                var loginResponseMessage = await client.PostAsync("login", loginContent);
-
-                // Read the results
-                var loginResult = await loginResponseMessage.Content.ReadAsStringAsync();
-
-                // Get the access token
-                // We could use string manipulation methods, but using JSON.NET is much easier
-                var accessToken = JToken.Parse(loginResult)["accessToken"].Value<string>();
-
-                // Add an authorization header to the list of default headers for our client
-                client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse($"Bearer {accessToken}");
-
-
-                // Let's attempt to retrieve the first page of alarms
-                var alarmsResponse = await client.GetAsync("alarms");
-
-                // Parse the response using Newtonsoft and print the first one.
-                var alarmsObject = JObject.Parse(await alarmsResponse.Content.ReadAsStringAsync());
-                var alarms = alarmsObject["items"];
-                Console.WriteLine($"First alarm: {alarms?[0]}");
+                var loginObject = new Login
+                {
+                    Username = username,
+                    Password = password
+                };
+                accessToken = (await client.Request("login").PostJsonAsync(loginObject)
+                    .ReceiveJson<LoginResponse>()).AccessToken;
             }
 
+            using (var client = new FlurlClient($"https://{hostname}/api/v2").WithOAuthBearerToken(accessToken))
+            {
+                var alarms = await client.Request("alarms").GetJsonAsync<AlarmCollection>();
+
+                Console.WriteLine($"Total alarms: {alarms.Total}");
+                Console.WriteLine($"First alarm: {JsonConvert.SerializeObject(alarms[5], Formatting.Indented)}");
+                Console.WriteLine($"Item reference of 3rd alarm: {alarms.Items[2].ItemReference}");
 
 
+                var secondPageOfAlarms = await client.Request(alarms.Next).GetJsonAsync<AlarmCollection>();
+                Console.WriteLine($"Name of object in second alarm on second page ${secondPageOfAlarms[1].Name}");
+            }
         }
-
     }
 }
